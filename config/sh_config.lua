bcc577712fdabb27c3d7eeaac511fb82db5b6eb0
MrAnderson <luketanderson1992@hotmail.co.uk>
2022-08-08 18:12:24 +0100

    convert-qb-input

--- config/sh_config.lua
+++ config/sh_config.lua
@@ -15,4 +15,10 @@ Config.jobName="warehouseman"
 
 Config.location = {
     [1] = {name="Adams_Apple" , coords= vector3(-43.81, -1236.47, 29.34),heading=270.41,minz=27.36 , maxz=30.87834,length=1,width=3,distance=2.5,debug=false},
+    [2] = {name="Only_ForTreyy" , coords= vector3(-45.52, -1275.12, 29.22),heading=270.41,minz=27.36 , maxz=30.87834,length=1,width=3,distance=2.5,debug=false},
+    [3] = {name="Shambles_St" , coords= vector3(1099.86, -2351.18, 30.27),heading=175.95,minz=27.36 , maxz=40.87834,length=1,width=3,distance=2.5,debug=false},
+    [4] = {name="Container_1" , coords= vector3(970.09, -1950.18, 30.86),heading=266.56,minz=27.36 , maxz=40.87834,length=1,width=3,distance=2.5,debug=false},
+    [5] = {name="Warehouse_1" , coords= vector3(1452.22, -1681.85, 65.94),heading=215.72,minz=20.36 , maxz=80.87834,length=1,width=3,distance=2.5,debug=false},
+    [6] = {name="Warehouse_2" , coords= vector3(1225.39, -1235.9, 35.53),heading=359.78,minz=30.36 , maxz=40.87834,length=1,width=3,distance=2.5,debug=false},
+    [7] = {name="Container_3" , coords= vector3(951.21, -1202.97, 25.87),heading=272.42,minz=20.36 , maxz=30.87834,length=1,width=3,distance=2.5,debug=false},
 }
